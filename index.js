125e48ee2d954645db02829ca5bf97b64f6381b3
PriitParmakson <priit.parmakson@gmail.com>
2018-02-17 19:38:55 +0200

    Metateave

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 'use strict';
 
 const express = require('express');
+const saml2 = require('saml2-js');
+
+// SP seadistused
+var sp_options = {
+  entity_id: "https://tere-demo.herokuapp.com/metadata",
+  private_key: fs.readFileSync("votmed/key-file.pem").toString(),
+  certificate: fs.readFileSync("votmed/cert-file.crt").toString(),
+  assert_endpoint: "https://tere-demo.herokuapp.com/assert",
+  force_authn: true,
+  auth_context: { comparison: "exact", class_refs: ["urn:oasis:names:tc:SAML:1.0:am:password"] },
+  nameid_format: "urn:oasis:names:tc:SAML:2.0:nameid-format:unspecified",
+  sign_get_request: true,
+  allow_unencrypted_assertion: true
+}
+
+// Moodusta SP 
+var sp = new saml2.ServiceProvider(sp_options);
 
 // Veebiserveri ettevalmistamine
 const app = express();
@@ -16,11 +33,23 @@ app.get('/', function (req, res) {
   res.render('pages/index');
 });
 
+// Metateabe otspunkt
+app.get('/metadata', (req, res) => {
+  // Moodusta metateabe XML
+  var metadata = sp.create_metadata();
+  res.header('Content-Type', 'text/xml').send(metadata);
+});
+
 // Autentimispäringu saatmine
 app.get('/auth', function (req, res) {
   // Autentimispäringute vastuvõtu otspunkt
   var suunaURL = 'https://eidastest.eesti.ee/EidasNode/ServiceProvider';
   res.redirect(suunaURL);
+});
+
+// Autentimisvastuse vastuvõtt
+app.post('/assert', function (req, res) {
+  res.send('Siin võetakse vastu autentimisvastus');
 });
 
 // Veebiserveri käivitamine
